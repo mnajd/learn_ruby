600f9dcec325978a64be01bc63af0ec50306518b
Don Najd <dnajd7@gmail.com>
2013-04-24 23:58:27 -0700

    vtable opening up array

--- lib/vtable/don_solution.rb
+++ lib/vtable/don_solution.rb
@@ -1,24 +1,33 @@
 puts 'VTable format'
 
-# data & desired columns
-data = (0...17).to_a
+class Array
+
+	def print_vtable(num_cols)
+
+		# calculate rows
+		num_rows = (self.length + num_cols - 1) / num_cols;   
+
+		# for each row
+		0.upto(num_rows-1) do  | current_row | 
+
+			# for each col
+			0.upto(num_cols - 1) do | current_col |  
+
+				# calculate index
+				index = (current_col * num_rows) + current_row
+
+				# add to row
+				print self[index].to_s.rjust(10)  
+
+			end
+			puts "\n"
+		end
+	end
+	
+end
+
+# cols
 num_cols = 3
 
-# calculate rows
-num_rows = (data.length + num_cols - 1) / num_cols;   
-
-# for each row
-0.upto(num_rows-1) do  | current_row | 
-
-	# for each col
-	0.upto(num_cols - 1) do | current_col |  
-
-		# calculate index
-		index = (current_col * num_rows) + current_row
-
-		# add to row
-		print data[index].to_s.rjust(10)  
-
-	end
-	puts "\n"
-end
+# print_vtable on array
+(0...17).to_a.print_vtable(num_cols)
--- lib/vtable/grace_solution.rb
+++ lib/vtable/grace_solution.rb
@@ -1,30 +1,37 @@
 puts 'VTable format'
 
-# data & desired columns
-data = (0...17).to_a
+class Array
+
+  def print_vtable(num_cols)
+    # calculate rows
+    num_rows = (self.length + (num_cols - 1)) / num_cols
+
+    # for each row
+    0.upto(num_rows-1) do |current_row|
+
+      # get index
+      index = current_row #zero based
+
+      # for each column
+      1.upto(num_cols) do |current_col|
+
+        # adjust the index
+        # add num_rows to index; if not first column
+        index += num_rows if current_col > 1
+
+        # print index
+        print self[index].to_s.rjust(10)
+
+      end
+
+      print "\n"
+
+    end
+  end
+end
+
+# cols
 num_cols = 3
 
-# calculate rows
-num_rows = (data.length + (num_cols - 1)) / num_cols
-
-# for each row
-0.upto(num_rows-1) do |current_row|
-
-  # get index
-  index = current_row #zero based
-
-  # for each column
-  1.upto(num_cols) do |current_col|
-
-    # adjust the index
-    # add num_rows to index; if not first column
-    index += num_rows if current_col > 1
-
-    # print index
-    print data[index].to_s.rjust(10)
-
-  end
-
-  print "\n"
-
-end
+# print_vtable on array
+(0...17).to_a.print_vtable(num_cols)
